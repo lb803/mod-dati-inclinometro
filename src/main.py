be9741f02d4053765b21bce30477d1d5b3dccc51
Luca Baffa <luca@openbookpublishers.com>
2021-11-08 17:46:04 +0000

    accept an input agument

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 from datetime import datetime
+import sys
+from os import path
 
-CAMPIONI = ['26-10-2021 02:00:00', '26-10-2021 05:00:00', '26-10-2021 08:00:00', '26-10-2021 11:00:00', '26-10-2021 14:00:00', '26-10-2021 17:00:00', '26-10-2021 20:00:00']
+file_path = path.abspath(sys.argv[1])
 
 def ordina_data(data):
     data_obj = datetime.strptime(data, '%d-%m-%Y %H:%M:%S')
@@ -9,8 +11,7 @@ def ordina_data(data):
     return data_obj.strftime('%d/%m/%Y %H:%M:%S')
 
 def main():
-    for data in CAMPIONI:
-        print(ordina_data(data))
+    print(file_path)
 
 
 if __name__ == "__main__":
