b96d73a68b9e070b62eae889ceb75f6dccc5c052
Luca Baffa <luca@openbookpublishers.com>
2021-11-08 18:28:27 +0000

    read cvs file and process date field

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@
 from datetime import datetime
 import sys
 from os import path
+import csv
+import re
 
 file_path = path.abspath(sys.argv[1])
 
@@ -11,7 +13,17 @@ def ordina_data(data):
     return data_obj.strftime('%d/%m/%Y %H:%M:%S')
 
 def main():
-    print(file_path)
+    with open(file_path) as csv_r_file:
+        data = csv.reader(csv_r_file, delimiter=';')
+
+        for row in data:
+            # if not empty and first column matches input date pattern
+            if len(row) and \
+               re.match(r'\d+-\d+-\d+ \d+:\d+:\d+', row[0]):
+                row[0] = ordina_data(row[0])
+                print(row[0])
+
+
 
 
 if __name__ == "__main__":
