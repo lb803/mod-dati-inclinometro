7288f68f5aaa299271112c4e5db7aab717186fa0
Luca Baffa <luca@openbookpublishers.com>
2021-11-08 18:36:33 +0000

    write oupput to file

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import csv
 import re
 
 file_path = path.abspath(sys.argv[1])
+file_name = path.basename(file_path)
 
 def ordina_data(data):
     data_obj = datetime.strptime(data, '%d-%m-%Y %H:%M:%S')
@@ -13,15 +14,20 @@ def ordina_data(data):
     return data_obj.strftime('%d/%m/%Y %H:%M:%S')
 
 def main():
-    with open(file_path) as csv_r_file:
-        data = csv.reader(csv_r_file, delimiter=';')
+    with open(file_name, 'w+') as csv_w_file:
+        writer = csv.writer(csv_w_file, delimiter=';')
+        
+        with open(file_path, 'r') as csv_r_file:
+            reader = csv.reader(csv_r_file, delimiter=';')
 
-        for row in data:
-            # if not empty and first column matches input date pattern
-            if len(row) and \
-               re.match(r'\d+-\d+-\d+ \d+:\d+:\d+', row[0]):
-                row[0] = ordina_data(row[0])
-                print(row[0])
+            for row in reader:
+                # if not empty and first column matches input date pattern
+                if len(row) and \
+                   re.match(r'\d+-\d+-\d+ \d+:\d+:\d+', row[0]):
+                    row[0] = ordina_data(row[0])
+
+                # write entire row to output file
+                writer.writerow(row)
 
 
 
