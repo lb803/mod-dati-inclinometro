9c48850b0c2e9d11401bac7a151448ee50263ec2
Luca Baffa <luca@openbookpublishers.com>
2021-11-17 18:14:29 +0000

    do not remove input files

--- src/mod-dati.sh
+++ src/mod-dati.sh
@@ -3,4 +3,4 @@
 set -eu
 
 find $INPUT -maxdepth 1 -type f -name "*.csv" -mmin +5 -exec \
-     sh -c 'sed "s,\([0-9]\{2\}\)-\([0-9]\{2\}\)-\([0-9]\{4\}\),\1/\2/\3," "$1" > $OUTPUT/$(basename "$1") && rm "$1"' sh {} \;
+     sh -c 'sed "s,\([0-9]\{2\}\)-\([0-9]\{2\}\)-\([0-9]\{4\}\),\1/\2/\3," "$1" > $OUTPUT/$(basename "$1")' sh {} \;
