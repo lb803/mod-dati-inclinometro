6459528f049b5a7ad9d3fc2fcef354046552208c
Luca Baffa <luca@openbookpublishers.com>
2021-11-24 18:03:41 +0000

    add example bat file

---
+++ example-bat-file.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\Luca\AppData\Local\Programs\Python\Python310\python.exe" C:\Users\Luca\Desktop\mod-dati-inclinometro\src\mod-dati.py C:\Users\Luca\Desktop\mod-dati-inclinometro\Va01 C:\Users\Luca\Desktop\mod-dati-inclinometro\out.csv
+exit
